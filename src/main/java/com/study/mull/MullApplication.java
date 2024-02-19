5246caabab4b1280d738d22a7e471763e934e8e2
d0nghee <hdh960326@gmail.com>
2024-02-19 19:19:17 +0900

    first commit

---
+++ src/main/java/com/study/mull/MullApplication.java
@@ -0,0 +1,13 @@
+package com.study.mull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/mull/MullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
