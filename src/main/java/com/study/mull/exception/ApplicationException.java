9de71e63142c60d55b234bf86374cc3bc2cf1532
d0nghee <hdh960326@gmail.com>
2024-02-20 19:33:43 +0900

    에러 처리

--- src/main/java/com/study/mull/exception/ApplicationException.java
+++ src/main/java/com/study/mull/exception/ApplicationException.java
@@ -1,4 +1,27 @@
 package com.study.mull.exception;
 
-public class ApplicationException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ApplicationException extends RuntimeException {
+
+    private ErrorCode errorCode;
+    private String message;
+
+    public ApplicationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
+
+    @Override
+    public String getMessage() {
+
+        if (message == null) {
+            return errorCode.getStatus().toString();
+        }
+
+        return String.format("%s, %s", errorCode, message);
+    }
 }
--- src/main/java/com/study/mull/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/study/mull/exception/GlobalExceptionAdvice.java
@@ -1,6 +1,85 @@
 package com.study.mull.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionAdvice {
+
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<?> applicationHandler(ApplicationException e) {
+        log.error("Error occurs {}", e.toString());
+        if (e.getErrorCode().equals(ErrorCode.CONNECTION_ERROR)) {
+            String errorMsg = "data: {\"type\": \"error\", \"message\": \"" + e.getErrorCode().getMessage()+ "\"}\n\n";
+            return ResponseEntity.status(e.getErrorCode().getStatus())
+                    .contentType(MediaType.parseMediaType("text/event-stream"))
+                    .body(errorMsg);
+        }
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(ErrorResponse.of(e.getErrorCode().name()));
+    }
+
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> applicationHandler(AuthenticationException e) {
+        log.error("Error occurs {}", e.toString());
+        // Unauthorized : 401번 인증 실패
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ErrorResponse.of(ErrorCode.UNAUTHORIZED.name()));
+    }
+
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> applicationHandler(RuntimeException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR.name()));
+    }
+
+
+
+
+
+
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException", e);
+        return ErrorResponse.of(e.getBindingResult());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+        log.error("ConstraintViolationException", e);
+        return ErrorResponse.of(e.getConstraintViolations());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleInvalidEnumValueException(HttpMessageNotReadableException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(ErrorCode.INVALID_ENUM_VALUE.name()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.error("MaxUploadSizeExceededException {}", e.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(ErrorCode.MAX_FILE_SIZE_10MB.name()));
+    }
 
 }
