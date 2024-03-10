820657f38cc41222e13417d55e8485b43e13146a
Jincchus <Jincchus@gmail.com>
2024-03-10 16:02:28 +0900

    content entity ing

---
+++ src/main/java/com/study/mull/content/entity/Content.java
@@ -0,0 +1,30 @@
+package com.study.mull.content.entity;
+
+import com.study.mull.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private long contentId;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member memberId;
+
+    private String contentCategory;
+
+    private String contentTitle;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime deleteAt;
+    private boolean deletedYn;
+}
