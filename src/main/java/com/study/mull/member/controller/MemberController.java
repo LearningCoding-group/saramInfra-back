3fd219700bd345cdc47e65a14681bd2820d3914d
d0nghee <hdh960326@gmail.com>
2024-03-17 14:24:24 +0900

    member test

---
+++ src/main/java/com/study/mull/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.study.mull.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Controller
+public class MemberController {
+
+
+}
